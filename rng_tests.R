fcb68f6f6527f6ae78c08135fd57943f8920b734
calebjmiller <cami1103@colorado.edu>
2018-08-28 13:22:13 -0600

    runs up/down test added

---
+++ rng_tests.R
@@ -0,0 +1,14 @@
+### Stochastic Simulation 
+# Tests for Random Number Generators
+# Runs Up/Down Test for Independence
+# Runs Above/Below the Mean Test for Independence
+# Length or Runs (Up/Down) Test for Independence
+# Autocorrelation Test for Independence
+# Chi-squared test for Uniformity
+# Serial Test
+# Kolmogorov-Smirnov Test
+### Caleb Miller
+
+# Set seed for reproducibility of results
+set.seed(1)
+
