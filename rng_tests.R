affed7d11c68167791b51f946f40eb4832cfe564
calebjmiller <cami1103@colorado.edu>
2018-08-29 10:59:19 -0600

    added runs above/below the mean test

--- rng_tests.R
+++ rng_tests.R
@@ -9,6 +9,52 @@
 # Kolmogorov-Smirnov Test
 ### Caleb Miller
 
+###################
+### Get Samples ###
+###################
+
 # Set seed for reproducibility of results
 set.seed(1)
+
+# Get a large sample of uniform random numbers
+n <-100000
+samples <-runif(n)
+
+#########################
+### Runs Up/Down Test ###
+#########################
+
+# Get R_n
+Rn<-0
+run<-matrix(0,(n-1),1)
+
+for (i in 1:(n-1)){
+  if(samples[i] < samples[i+1]){
+    run[i]=1
+  }
+}
+for (i in 1:(n-2)){
+  if(run[i] != run[i+1]){
+    Rn<-Rn+1
+  }
+}
+
+# Get E[R_n]
+ERn <- (2*n-1)/3
+
+# Get Var[Rn]
+VarRn <- (16*n-29)/90
+
+# Get Z_n
+Zn = (Rn-ERn)/sqrt(VarRn)
+
+# Result of Test
+if(-1.96<Zn & Zn< 1.96){
+  print("This sample has passed the runs up and down test for independence")
+} else{
+  print("This sample has passed the runs up and down test for independence")
+}
+
+
+
 
