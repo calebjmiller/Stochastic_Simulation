f714ee34c58fa065713064bfb6ca046c5d382292
calebjmiller <cami1103@colorado.edu>
2018-09-13 12:24:02 -0600

    added autocorrelation,chi-squared, and serial test

--- rng_tests.R
+++ rng_tests.R
@@ -1,5 +1,5 @@
 ### Stochastic Simulation 
-# Tests for Random Number Generators
+### Tests for Random Number Generators
 # Runs Up/Down Test for Independence
 # Runs Above/Below the Mean Test for Independence
 # Length or Runs (Up/Down) Test for Independence
@@ -136,7 +136,7 @@ for (i in 1:6){
 
 O[6] <- O[6] + sum(lengthu == 7) + sum(lengthu == 8) + sum(lengthu == 9)+sum(lengthd == 7) + sum(lengthd == 8) + sum(lengthd == 9)
 
-W = sum(((O-E)^2)/E)
+W <- sum(((O-E)^2)/E)
 
 # Result of Test
 if(W< 11.075){
@@ -144,3 +144,114 @@ if(W< 11.075){
 } else{
   print("This sample has not passed the length of runs test for independence")
 }
+
+#############################
+### Auto Correlation Test ###
+#############################
+
+# lag
+j<-1
+# h
+h<-n-j
+# get lag product sum
+lps<-0
+
+for (i in 1:(n-j)){
+lps <- lps + samples[i]*samples[i+j]
+}
+
+#\hat{p}_j
+phat <- 12*lps/h-3
+
+#\sigma_n^2
+sigma_sq <- 144/h^2*var(lps)
+
+#sigma_n
+sigma <- sqrt(sigma_sq)
+
+#Z_n
+Z = phat/sigma
+
+# Result of Test
+if(-1.96<Zn & Zn< 1.96){
+  print("This sample has passed the autocorrelation test for independence")
+} else{
+  print("This sample has not passed the autocorrelation test for independence")
+}
+
+
+########################
+### Chi-squared Test ###
+########################
+
+# number of bins
+nbins <- 20
+
+# expected in the bin
+E <- n/nbins
+
+# how many observed in each bin
+O <- matrix(0,1,nbins)
+
+for (i in 1:nbins){
+  O[i]<-sum(((i-1)/nbins)< samples & samples <(i/nbins))
+}
+
+# Get W
+W <- sum(((O-E)^2)/E)
+
+# Result
+if(W< 30.144){
+  print("This sample has passed the chi-squared test for uniformity")
+} else{
+  print("This sample has failed the chi-squared test for uniformity")
+}
+
+###################
+### Serial Test ###
+###################
+
+# dimension 
+d <- 2
+
+# nbins
+nbinsx <- 20
+nbinsy <- 20
+nbins <- nbinsx*nbinsy
+
+# coordinates
+x <- samples[seq(1,n,2)]
+y <- samples[seq(2,n,2)]
+
+# Expected
+E <- n/(2*nbins)
+
+# Observed
+O <- matrix(0,1,nbins)
+
+k=1
+
+for (i in 1:nbinsx){
+  for(j in 1:nbinsy){
+    O[k]=sum(((i-1)/nbinsx)<x & x<(i/nbinsx) & ((j-1)/nbinsy)<y & y<(j/nbinsy))
+    k=k+1
+  }
+}
+
+# Get W
+W <- sum(((O-E)^2)/E)
+
+
+# Result
+if(W< 446.5742){
+  print("This sample has passed the serial test for uniformity")
+} else{
+  print("This sample has failed the serial test for uniformity")
+}
+
+###############################
+### Kolmogorov-Smirnov Test ###
+###############################
+
+
+
